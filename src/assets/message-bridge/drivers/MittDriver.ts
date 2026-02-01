5eff3c413e1b5bfa55d2babd729945444e837b16
wuyax <pengqingkai@gmail.com>
2026-02-01 18:19:44 +0800

    refactor: introduce `eventIndicator` constant for Mitt event name

--- src/assets/message-bridge/drivers/MittDriver.ts
+++ src/assets/message-bridge/drivers/MittDriver.ts
@@ -1,16 +1,18 @@
 import type { Emitter } from 'mitt'
 import BaseDriver, { type Message } from './BaseDriver'
 
+const eventIndicator = 'message_bridge_message_event'
+
 export default class MittDriver extends BaseDriver {
   emitter: Emitter<Record<string, Message>>
   constructor(emitter: Emitter<Record<string, Message>>) {
     super()
     this.emitter = emitter
-    this.emitter.on('message', (data) => {
+    this.emitter.on(eventIndicator, (data) => {
       if (data) this.onMessage?.(data)
     })
   }
   send(data: Message) {
-    this.emitter.emit('message', data)
+    this.emitter.emit(eventIndicator, data)
   }
 }
